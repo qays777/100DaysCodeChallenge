e8438ce801d21ef9e251c597aaad283a629ab2e2
Qays Arkan <89450660+qays777@users.noreply.github.com>
2024-09-20 20:45:44 +0700

    day1

--- day001.js
+++ day001.js
@@ -1,6 +1,5 @@
 /* 
 
-Description:
 Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String).
 
 Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
@@ -17,10 +16,3 @@ Examples:
   function hello(name) {
     return `Hello, ${name ? name[0].toUpperCase() + name.slice(1).toLowerCase() : "World"}!`
   }
-
-
-// pemanggilan fungsi
-console.log(hello("john"));
-console.log(hello("aliCE"));
-console.log(hello(""));
-console.log(hello());
